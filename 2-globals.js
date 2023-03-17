0daa9dd676e77ac426adef4c5f0ea1d1fafeecea
RamonRMG <98474605+RamonRMG@users.noreply.github.com>
2023-03-17 09:38:46 +0000

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,8 @@
+const secret = 'SUPER SECRET'
+const John = 'John'
+const Peter = 'Peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// local
+const secret = 'SUPER SECRET'
+//share
+const John = 'John'
+const Peter = 'Peter'
+
+
+module.exports = {John, Peter}
